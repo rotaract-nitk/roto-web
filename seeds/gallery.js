f534211fb0e5fa1843b771dab93acd04d1268b6a
Olan-Pinto <olanpinto@gmail.com>
2022-01-20 10:01:31 +0530

    Gallery seeds added

--- seeds/gallery.js
+++ seeds/gallery.js
@@ -1,14 +1,57 @@
 module.exports = [
     {
         name: "img1",
-        imageURL: "https://res.cloudinary.com/djlnz87ow/image/upload/v1642563493/rotoWebsite/3_vufy00.jpg"
+        imageURL: "https://res.cloudinary.com/djlnz87ow/image/upload/v1642651885/rotoWebsite/Copy_of_IMG_1629_ibrioq.heic"
     },
     {
         name: "img2",
-        imageURL: "https://res.cloudinary.com/djlnz87ow/image/upload/v1642563463/rotoWebsite/2_skpfy9.jpg"
+        imageURL: "https://res.cloudinary.com/djlnz87ow/image/upload/v1642651881/rotoWebsite/IMG_1625_xsaxrm.heic"
     },
     {
         name: "img3",
-        imageURL: "https://res.cloudinary.com/djlnz87ow/image/upload/v1642563437/rotoWebsite/1_bpw2gx.jpg"
+        imageURL: "https://res.cloudinary.com/djlnz87ow/image/upload/v1642651905/rotoWebsite/20200308_130729_ceyfcm.jpg"
+    },
+
+    {
+        name:"img4",
+        imageURL:"https://res.cloudinary.com/djlnz87ow/image/upload/v1642651902/rotoWebsite/DSC_4224_fn36gz.jpg"
+    },
+        {
+        name:"img5",
+        imageURL:"https://res.cloudinary.com/djlnz87ow/image/upload/v1642651897/rotoWebsite/DSC_4210_wl5mnr.jpg"
+    },
+        {
+        name:"img6",
+        imageURL:"https://res.cloudinary.com/djlnz87ow/image/upload/v1642651898/rotoWebsite/DSC_4242_l7usur.jpg"
+    },
+
+    {
+        name:"img7",
+        imageURL:"https://res.cloudinary.com/djlnz87ow/image/upload/v1642563463/rotoWebsite/2_skpfy9.jpg"
+    },
+
+    {
+        name:"img8",
+        imageURL:"https://res.cloudinary.com/djlnz87ow/image/upload/v1642651877/rotoWebsite/IMG-20191013-WA0004_lr6ln3.jpg"
+    },
+
+    {
+        name:"img9",
+        imageURL:"https://res.cloudinary.com/djlnz87ow/image/upload/v1642651877/rotoWebsite/WhatsApp_Image_2018-07-24_at_9.27.21_PM_empyvj.jpg"
+    },
+
+    {
+        name:"img10",
+        imageURL:"https://res.cloudinary.com/djlnz87ow/image/upload/v1642651918/rotoWebsite/IMG_5704_ikmye9.jpg"
+    },
+
+    {
+        name:"img11",
+        imageURL:"https://res.cloudinary.com/djlnz87ow/image/upload/v1642651883/rotoWebsite/CLUB_MEET1_z6w7dh.png"
+    },
+
+    {
+        name:"img12",
+        imageURL:"https://res.cloudinary.com/djlnz87ow/image/upload/v1642651883/rotoWebsite/IMG_20200221_181101_gtnoaj.jpg"
     }
 ];
